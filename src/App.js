3342d74820697575cc2e3f76e1e21e8f85b3dde5
Arif Khan <arifurrahmankhan0@gmail.com>
2021-03-10 19:18:48 +0800

    minimal setup for todo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//custom components
+import Todos from './Todos';
+import AddTodo from './AddTodo';
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'Watch anime'},
+      {id:2, content: 'Watch tutorials'}
+    ]
+  }
+
+  deleteTodo = (id) =>{
+    const todos = this.state.todos.filter(todo =>{
+      return todo.id !== id;
+    });
+
+    this.setState({
+      todos: todos
+    });
+
+  }
+  addTodo = (todo) =>{
+    todo.id = parseInt(Math.random()*100 + 2);
+    let todos = [...this.state.todos, todo]
+    this.setState({
+      todos
+    })
+  }
+
+  render(){
+    return(
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo} />
+      </div>
+    );
+  }
 }
 
 export default App;
